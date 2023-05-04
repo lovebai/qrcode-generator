d45464850f075fe0f19ae74aa448f84cf4fdbf42
lovebai <1507820372@qq.com>
2023-05-04 13:32:50 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Card, Tabs, Input } from "antd";
+import QRCode from "qrcode.react";
 
-function App() {
+const { TextArea } = Input;
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [selectedKey, setSelectedKey] = useState("1");
+  const [temp, setTemp] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleInputChange1 = (e) => {
+    setTemp(e.target.value);
+  };
+
+  const handleTabChange = (key) => {
+    setSelectedKey(key);
+    setInputValue("");
+  };
+
+  const handleKey4=()=>{
+    if (selectedKey==="3"){
+      return `tel:${inputValue}`
+    }else if(selectedKey==="4"){
+      // if(/^\d{3,16}$/.test(inputValue)){
+      //   setTemp(inputValue)
+      // }
+     return`sms:${inputValue}?body=${encodeURIComponent(temp)}`
+    }else {
+      return inputValue
+    }
+
+  }
+
+  // const qrValue =
+  //   selectedKey === "3"
+  //     ? `tel:${inputValue}`
+  //     : selectedKey === "4"
+  //     ? `smsto:${inputValue}:${inputValue}`
+  //     : inputValue;
+  const qrValue = handleKey4()
+
+  const tabItems = [
+    {
+      key: "1",
+      label: "文本",
+      children: (
+        <div>
+        <TextArea
+          rows={6}
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="请输入文本内容"
+        />
+        </div>
+        
+      ),
+    },
+    {
+      key: "2",
+      label: "链接",
+      children: (
+        <div>
+        <Input
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="请输入链接地址"
+        />
+        </div>
+        
+      ),
+    },
+    {
+      key: "3",
+      label: "拨号",
+      children: (
+        <div>
+        <Input
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="请输入电话号码"
+        />
+        </div>
+        
+      ),
+    },
+     {
+      key: "4",
+      label: "短信",
+      disabled:false,
+      children: (
+        <div>
+        <Input
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="请输入电话号码"
+        />
+        <TextArea rows={6} placeholder="请输入短信内容"
+        value={temp}
+        onChange={handleInputChange1}
+        style={{marginTop:'1rem'}} />
+        </div>
+      ),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        margin: "24px",
+      }}
+    >
+      <Card style={{ width: "100%" }}>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+        <Tabs
+          style={{ width: "60%" }}
+          defaultActiveKey="1"
+          activeKey={selectedKey}
+          onChange={handleTabChange}
+          items={tabItems}
+        />
+        <div style={{ width: "50%", paddingLeft: "16px" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <QRCode value={qrValue} size={256} />
+            </div>
+          </div>
+        </div>
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
